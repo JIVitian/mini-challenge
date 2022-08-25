06e1baf5f5032284f2149d8a7b02a49c144f2f31
JIVitian <jivitian@gmail.com>
2022-08-24 22:48:53 -0300

    First try of table

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { Person } from './models/person';
+
+const mockData: Person[] = [
+  {
+    nombre: 'Pedro',
+    edad: 20,
+    carrera: 'Ingeniería',
+    hobbie: 'Futbol',
+  },
+  {
+    nombre: 'Rodrigo',
+    edad: 22,
+    carrera: 'Arquitectura',
+    hobbie: 'Bajo eléctrico',
+  },
+  {
+    nombre: 'Romina',
+    edad: 21,
+    carrera: 'Abogacía',
+    hobbie: 'Acrobacia',
+  },
+  {
+    nombre: 'Ana',
+    edad: 23,
+    carrera: 'Contadora',
+    hobbie: 'Astronomía',
+  },
+];
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  people = mockData;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
---
+++ src/app/home/models/person.ts
@@ -0,0 +1,7 @@
+export interface Person {
+  id?: number;
+  edad: number;
+  nombre: string;
+  carrera: string;
+  hobbie: string;
+}
